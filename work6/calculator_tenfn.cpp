371f5d6db3df195de910d982e3e58addd6876997
NopXx <khamkokaewn1@hotmail.com>
2023-09-19 11:52:04 +0700

    Update calculator_tenfn.cpp

---
+++ work6/calculator_tenfn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+void add(float &sum, float num1, float num2);
+void subtract(float &sum, float num1, float num2);
+void multiply(float &sum, float num1, float num2);
+void divide(float &sum, float num1, float num2);
+void mod(float &sum, float num1, float num2);
+void power(float &sum, float num1, float num2);
+void circle(float &sum, float num1);
+
+
+int main() {
+    
+    return 0;
+}
