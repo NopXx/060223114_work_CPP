ffa60afc7ab8b28bb91dce677a4861db338915c6
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-09 21:11:20 +0700

    Update lab1.cpp

--- work7/lab1.cpp
+++ work7/lab1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 string full_month(int n);
 string short_month(int n);
 string Format_one(string date);
 string Format_two(string date);
-void stop_day(int month, int day);
+void stop_day(string date);
+string ps_stop_day(string date, string day_input, string month_input);
 
 int main()
 {
@@ -14,10 +16,11 @@ int main()
 
     cout << "Input Date(dd/mm/yyyy) : ";
     cin >> date;
-    
-    cout <<"Format 1: " << Format_one(date) << endl;
-    cout <<"Format 2: " << Format_two(date) << endl;
-    stop_day(10,3);
+
+    cout << "Format 1: " << Format_one(date) << endl;
+    cout << "Format 2: " << Format_two(date) << endl;
+    stop_day(date);
+
     return 0;
 }
 
@@ -33,7 +36,29 @@ string full_month(int n)
     return month[n - 1];
 }
 
-void stop_day(int month, int day) {
+void stop_day(string date)
+{
+    string new_date[3], format;
+    int index = 0;
+    for (int i = 0; i < date.length(); i++)
+    {
+        if (date[i] != '/')
+        {
+            new_date[index] += date[i];
+        }
+        else
+        {
+            index++;
+        }
+    }
+    cout << (ps_stop_day(date, new_date[0], new_date[1]) != "" ? 
+        ps_stop_day(date, new_date[0], new_date[1]) : 
+        new_date[0] + " " + short_month(stoi(new_date[1])) + " is not Holiday");
+}
+
+string ps_stop_day(string date, string day_input, string month_input)
+{
+    string result = "";
     string stop_day[][12] = {
         {"01/New Year Holiday", "02/new Year Day Substitution Holiday"},
         {},
@@ -47,35 +72,48 @@ void stop_day(int month, int day) {
         {"13/King Naresuan Day", "23/Chulalongkorn Day"},
         {},
         {"05/Father's Day", "10/Constitution Day", "11/Constitution Day substitution day", "31/End Year Day"}};
-string new_date[3];
-   int index = 0; 
-     for (int i = 0; i < date.length(); i++) 
-     { 
-         if (date[i] != '/') 
-         { 
-             new_date[index] += date[i]; 
-         } 
-         else { 
-             index++; 
-         } 
-     }
-     int month = stoi(new_date[1]);
-     for (int i = 0; i < 12; i++) {
-         string day = stop_day[month-1][i];
-         string temp_day[2];
-         int index = 0;
-         for (int j = 0; j < day.length(); j++) {
-             if (day[i] != '/') {
-                 temp_day[index] += day[i];
-             } else {
-                 index++;
-             }
-             
-         }
-         if (temp_day[0] == new_date[0]) {
-             cout << temp_day[0] << " : " << endl;
-         }
-     }
+    int month = stoi(month_input);
+    string day[] = {};
+    int index_day = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        if (!stop_day[month - 1][i].empty())
+        {
+            day[index_day] = stop_day[month - 1][i];
+            index_day++;
+        }
+    }
+    string temp_day[2];
+    int index_j = 0;
+    for (int j = 0; j < index_day; j++)
+    {
+        string temp = day[j];
+        for (int n_str = 0; n_str < temp.length(); n_str++)
+        {
+
+            if (temp[n_str] != '/')
+            {
+                temp_day[index_j] += temp[n_str];
+            }
+            else
+            {
+                index_j++;
+            }
+        }
+        if (stoi(temp_day[0]) == stoi(day_input))
+        {
+
+            result = temp_day[0] + " " + short_month(month) + " is " + temp_day[1];
+            break;
+        }
+        else
+        {
+            index_j = 0;
+            temp_day[0] = "";
+            temp_day[1] = "";
+        }
+    }
+    return result;
 }
 
 string Format_one(string date)
@@ -88,12 +126,14 @@ string Format_one(string date)
         {
             new_date[index] += date[i];
         }
-        else {
+        else
+        {
             index++;
         }
     }
     int month = stoi(new_date[1]);
     format += full_month(month) + " " + new_date[0] + ", " + new_date[2];
+
     return format;
 }
 
@@ -108,7 +148,8 @@ string Format_two(string date)
         {
             new_date[index] += date[i];
         }
-        else {
+        else
+        {
             index++;
         }
     }
