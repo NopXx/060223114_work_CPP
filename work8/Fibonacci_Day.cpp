d966de39f21f2fa8c4499555a873edbff5dfb7f8
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-11 22:06:58 +0700

    Update Fibonacci_Day.cpp

--- work8/Fibonacci_Day.cpp
+++ work8/Fibonacci_Day.cpp
@@ -4,23 +4,49 @@ using namespace std;
 
 int main()
 {
-    int n; 
-    int fib[n] = {0, 1};
+    int n;
 
     cout << "Enter Number : ";
     cin >> n;
-    for (int i = 2; i < n; i++) {
+
+    if (n <= 2)
+    {
+        if (n == 0)
+        {
+            cout << 0 << " is Fibonacci at F0" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << 1 << " is Fibonacci at F1 and F2" << endl;
+            return 0;
+        } 
+    }
+
+    int fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n + 1; i++)
+    {
         fib[i] = fib[i - 1] + fib[i - 2];
-        cout << "Fib[" << i << "] = " << fib[i] << endl;
-        if (fib[i] == n) {
-            cout << n << " is Fibonacci" << endl;
-        } else {
-            if (fib[i] > n) {
+    }
+
+    for (int i = 0; i <= n + 1; i++)
+    {
+        if (fib[i] == n)
+        {
+            cout << n << " is Fibonacci at F" << i << endl;
+            break;
+        }
+        else
+        {
+            if (fib[i] > n)
+            {
                 cout << n << " is not Fibonacci" << endl;
                 break;
             }
         }
     }
-
     return 0;
 }
