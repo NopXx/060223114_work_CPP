0844c85d4e2483a81772a60e61910e78e205a913
NopXx <97354576+NopXx@users.noreply.github.com>
2023-10-13 19:57:56 +0700

    update add fn checkday

--- work8/Fibonacci_Day.cpp
+++ work8/Fibonacci_Day.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
 
 using namespace std;
-
+void checkDay(int n);
 int main()
 {
     int n;
 
     cout << "Enter Number : ";
     cin >> n;
+    checkDay(n);
+    return 0;
+}
 
-    if (n <= 2)
-    {
-        if (n == 0)
-        {
+void checkDay(int n)
+{
+    if (n <= 2) {
+        if (n == 0) {
             cout << 0 << " is Fibonacci at F0" << endl;
-            return 0;
-        }
-        else
-        {
+        } else {
             cout << 1 << " is Fibonacci at F1 and F2" << endl;
-            return 0;
-        } 
-    }
-
-    int fib[n + 1];
-    fib[0] = 0;
-    fib[1] = 1;
-
-    for (int i = 2; i <= n + 1; i++)
-    {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    for (int i = 0; i <= n + 1; i++)
-    {
-        if (fib[i] == n)
-        {
-            cout << n << " is Fibonacci at F" << i << endl;
-            break;
         }
-        else
-        {
-            if (fib[i] > n)
-            {
-                cout << n << " is not Fibonacci" << endl;
+    } else {
+        int fib[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n + 1; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        for (int i = 0; i <= n + 1; i++) {
+            if (fib[i] == n) {
+                cout << n << " is Fibonacci at F" << i << endl;
                 break;
+            } else {
+                if (fib[i] > n) {
+                    cout << n << " is not Fibonacci" << endl;
+                    break;
+                }
             }
         }
     }
-    return 0;
 }
